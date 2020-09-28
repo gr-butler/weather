0d06154baf7a41ee33d5adc51054b564fd8d93a9
gary.butler <gary.butler@hpe.com>
2020-09-28 20:29:26 +0100

    panics when serving

--- bme280/bme280.go
+++ bme280/bme280.go
@@ -1,16 +1,9 @@
 package bme280
 
 import (
-	"fmt"
-
 	"github.com/d2r2/go-bsbmp"
 	"github.com/d2r2/go-i2c"
-	logger "github.com/d2r2/go-logger"
-)
-
-var lg = logger.NewPackageLogger("main",
-	logger.DebugLevel,
-	// logger.InfoLevel,
+	logger "github.com/sirupsen/logrus"
 )
 
 //Bme280 the sensor
@@ -18,64 +11,62 @@ type Bme280 struct {
 	I2cbus *i2c.I2C
 }
 
-func (b *Bme280) Read() string {
+type sensordata struct {
+	Temp float64 
+	Humidity float64 
+	Pressure float64 
+	PressureHg float64
+}
 
-	// sensor, err := bsbmp.NewBMP(bsbmp.BMP180, i2c) // signature=0x55
-	// sensor, err := bsbmp.NewBMP(bsbmp.BMP280, i2c) // signature=0x58
-	sensor, err := bsbmp.NewBMP(bsbmp.BME280, b.I2cbus) // signature=0x60
-	// sensor, err := bsbmp.NewBMP(bsbmp.BMP388, i2c) // signature=0x50
+func (b *Bme280) Read() *sensordata {
+
+	sensor, err := bsbmp.NewBMP(bsbmp.BME280, b.I2cbus) 
 	if err != nil {
-		lg.Fatal(err)
+		logger.Fatal(err)
 	}
 
-	id, err := sensor.ReadSensorID()
+	_, err = sensor.ReadSensorID()
 	if err != nil {
-		lg.Fatal(err)
+		logger.Fatal(err)
 	}
-	lg.Infof("This Bosch Sensortec sensor has signature: 0x%x", id)
 
 	err = sensor.IsValidCoefficients()
 	if err != nil {
-		lg.Fatal(err)
+		logger.Fatal(err)
 	}
 
 	// Read temperature in celsius degree
 	t, err := sensor.ReadTemperatureC(bsbmp.ACCURACY_STANDARD)
 	if err != nil {
-		lg.Fatal(err)
+		logger.Fatal(err)
 	}
-	lg.Infof("Temprature = %v*C", t)
 
 	// Read atmospheric pressure in pascal
 	p, err := sensor.ReadPressurePa(bsbmp.ACCURACY_LOW)
 	if err != nil {
-		lg.Fatal(err)
+		logger.Fatal(err)
 	}
-	lg.Infof("Pressure = %v Pa", p)
 
 	// Read atmospheric pressure in mmHg
 	pm, err := sensor.ReadPressureMmHg(bsbmp.ACCURACY_LOW)
 	if err != nil {
-		lg.Fatal(err)
+		logger.Fatal(err)
 	}
-	lg.Infof("Pressure = %v mmHg", pm)
 
 	// Read atmospheric pressure in mmHg
 	supported, h1, err := sensor.ReadHumidityRH(bsbmp.ACCURACY_LOW)
 	if supported {
 		if err != nil {
-			lg.Fatal(err)
+			logger.Fatal(err)
 		}
-		lg.Infof("Humidity = %v %%", h1)
 	}
 
-	// Read atmospheric altitude in meters above sea level, if we assume
-	// that pressure at see level is equal to 101325 Pa.
-	a, err := sensor.ReadAltitude(bsbmp.ACCURACY_LOW)
-	if err != nil {
-		lg.Fatal(err)
-	}
-	lg.Infof("Altitude = %v m", a)
+	logger.Infof("Temp [%v], Hum [%v], Pressure [%v]Pa ([%v] mmHg)", t, h1, p, pm) // add timestamp?
 
-	return fmt.Sprintf("Temp [%v], Hum [%v], Pressure [%v]Pa ([%v] mmHg)", t, h1, p, pm)
+	return &sensordata{
+		Temp: float64(t),
+		Humidity: float64(h1),
+		Pressure: float64(p),
+		PressureHg: float64(pm),
+	}
 }
--- main.go
+++ main.go
@@ -1,106 +1,121 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"periph.io/x/periph/conn/i2c/i2creg"
 	"periph.io/x/periph/experimental/devices/mcp9808"
 	"periph.io/x/periph/host"
 
-	"./bme280"
-
 	"github.com/d2r2/go-i2c"
-	logger "github.com/d2r2/go-logger"
+	"github.com/pointer2null/weather/bme280"
+
+	d2logger "github.com/d2r2/go-logger"
+	logger "github.com/sirupsen/logrus"
 )
 
-var lg = logger.NewPackageLogger("main",
-	logger.DebugLevel,
-	// logger.InfoLevel,
-)
 
 type sensors struct {
 	myBme *bme280.Bme280
 }
 
+type webdata struct {
+	Temp float64 `json:"temp"`
+	Temp1 float64 `json:"temp1"`
+	Humidity float64 `json:"humidity"`
+	Pressure float64 `json:"pressure"`
+}
+
 func main() {
-	defer logger.FinalizeLogger()
+	// temp junk to stop log spam by d2r2
+	defer d2logger.FinalizeLogger()
+	d2logger.ChangePackageLogLevel("i2c", d2logger.InfoLevel)
+	d2logger.ChangePackageLogLevel("bsbmp", d2logger.InfoLevel)
+
+
 	// Create new connection to i2c-bus on 1 line with address 0x76.
 	// Use i2cdetect utility to find device address over the i2c-bus
 	myi2c, err := i2c.NewI2C(0x76, 1)
 	if err != nil {
-		lg.Fatal(err)
+		logger.Fatal(err)
 	}
 	defer myi2c.Close()
 
 	b := bme280.Bme280{I2cbus: myi2c}
 	s := sensors{myBme: &b}
 
-	// Uncomment/comment next lines to suppress/increase verbosity of output
-	logger.ChangePackageLogLevel("i2c", logger.InfoLevel)
-	logger.ChangePackageLogLevel("bsbmp", logger.InfoLevel)
-
 	http.HandleFunc("/", s.handler)
 	log.Fatal(http.ListenAndServe(":80", nil))
-	lg.Info("Starting mcp9808 reader...")
-	foo()
+	logger.Info("Starting mcp9808 reader...")
+	
 }
 
 func (s *sensors) handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, s.myBme.Read())
+	sd := s.myBme.Read()
+
+	t1 := getTemp()
+
+	wd := webdata {
+		Temp: sd.Temp,
+		Temp1: t1,
+		Humidity: sd.Humidity,
+		Pressure: sd.Pressure,
+	}
+	json.NewEncoder(w).Encode(wd)
 }
 
-func foo() error {
+func getTemp() float64 {
 	if _, err := host.Init(); err != nil {
-		return err
+		logger.Fatal(err)
 	}
 	address := flag.Int("address", 0x18, "I²C address")
 	i2cbus := flag.String("bus", "", "I²C bus (/dev/i2c-1)")
 
 	flag.Parse()
 
-	lg.Info("Starting MCP9808 Temperature Sensor")
+	logger.Info("Starting MCP9808 Temperature Sensor")
 	if _, err := host.Init(); err != nil {
-		return err
+		logger.Fatal(err)
 	}
 
 	// Open default I²C bus.
 	bus, err := i2creg.Open(*i2cbus)
 	if err != nil {
-		return fmt.Errorf("failed to open I²C: %v", err)
+		logger.Fatalf("failed to open I²C: %v", err)
 	}
 	defer bus.Close()
 
 	// Create a new temperature sensor a sense with default options.
 	sensor, err := mcp9808.New(bus, &mcp9808.Opts{Addr: *address})
 	if err != nil {
-		return fmt.Errorf("failed to open new sensor: %v", err)
+		logger.Fatalf("failed to open new sensor: %v", err)
 	}
+
+
+	t, err := sensor.SenseTemp()
 
 	// Read values from sensor every second.
-	everySecond := time.Tick(time.Second)
-	var halt = make(chan os.Signal, 1)
-	signal.Notify(halt, syscall.SIGTERM)
-	signal.Notify(halt, syscall.SIGINT)
+	// everySecond := time.Tick(time.Second)
+	// var halt = make(chan os.Signal, 1)
+	// signal.Notify(halt, syscall.SIGTERM)
+	// signal.Notify(halt, syscall.SIGINT)
 
-	lg.Info("ctrl+c to exit")
-	for {
-		select {
-		case <-everySecond:
-			t, err := sensor.SenseTemp()
-			if err != nil {
-				return fmt.Errorf("sensor reading error: %v", err)
-			}
-			lg.Info(t)
+	// logger.Info("ctrl+c to exit")
+	// for {
+	// 	select {
+	// 	case <-everySecond:
+	// 		t, err := sensor.SenseTemp()
+	// 		if err != nil {
+	// 			return logger.Fatalf("sensor reading error: %v", err)
+	// 		}
+	// 		logger.Info(t)
 
-		case <-halt:
-			return nil
-		}
-	}
+	// 	case <-halt:
+	// 		return nil
+	// 	}
+	// }
+	return t.Celsius()
 }
