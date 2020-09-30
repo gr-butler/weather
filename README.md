23772f232f0c75e6f42131892f26f12d4d28b498
gary.butler <gary.butler@hpe.com>
2020-09-30 17:38:19 +0100

    Moved to periph.io library
    Removed r2d2 libs and my BME280 package

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # weather
 Raspberry Pi weather station
+
+
+ env GOARCH=arm GOARM=5 GOOS=linux go build -o weatherServer.exe
--- main.go
+++ main.go
@@ -3,73 +3,72 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"log"
 	"net/http"
 
+	"periph.io/x/periph/conn/i2c"
 	"periph.io/x/periph/conn/i2c/i2creg"
+	"periph.io/x/periph/conn/physic"
+	"periph.io/x/periph/devices/bmxx80"
 	"periph.io/x/periph/experimental/devices/mcp9808"
 	"periph.io/x/periph/host"
 
-	"github.com/d2r2/go-i2c"
-	"github.com/pointer2null/weather/bme280"
-
-	d2logger "github.com/d2r2/go-logger"
 	logger "github.com/sirupsen/logrus"
 )
 
 
 type sensors struct {
-	myBme *bme280.Bme280
 	mcp *mcp9808.Dev
+	bme *bmxx80.Dev
 }
 
 type webdata struct {
-	Temp float64 `json:"temp"`
-	Temp1 float64 `json:"temp1"`
-	Humidity float64 `json:"humidity"`
-	Pressure float64 `json:"pressure"`
+	Temp float64 `json:"temp_C"`
+	Temp1 float64 `json:"temp1_C"`
+	Humidity float64 `json:"humidity_RH"`
+	Pressure float64 `json:"pressure_hPa"`
 }
 
+
+
 func main() {
-	// temp junk to stop log spam by d2r2
-	defer d2logger.FinalizeLogger()
-	d2logger.ChangePackageLogLevel("i2c", d2logger.InfoLevel)
-	d2logger.ChangePackageLogLevel("bsbmp", d2logger.InfoLevel)
 
-
-	// Create new connection to i2c-bus on 1 line with address 0x76.
-	// Use i2cdetect utility to find device address over the i2c-bus
-	myi2c, err := i2c.NewI2C(0x76, 1)
-	if err != nil {
-		logger.Fatal(err)
-	}
-	defer myi2c.Close()
-
-	b := bme280.Bme280{I2cbus: myi2c}
-	m := initMCP9808()
-	s := sensors{myBme: &b, mcp: m}
-
+	logger.Info("Initialize sensors...")
+	m, d, bus := initMCP9808()
+	defer bus.Close()
+	s := sensors{bme: d, mcp: m}
 	http.HandleFunc("/", s.handler)
-	log.Fatal(http.ListenAndServe(":80", nil))
-	logger.Info("Starting mcp9808 reader...")
+	logger.Info("Starting webservice...")
+	logger.Fatal(http.ListenAndServe(":80", nil))
 	
+	logger.Info("Exiting...")
 }
 
 func (s *sensors) handler(w http.ResponseWriter, r *http.Request) {
-	sd := s.myBme.Read()
+	w.Header().Set("Content-Type", "application/json")
+	e := physic.Env{}
+	s.mcp.Sense(&e)
+	logger.Debugf("MCP: %8s %10s %9s\n", e.Temperature, e.Pressure, e.Humidity)
 
-	t1 := getTemp(s.mcp)
-
+	em := physic.Env{}
+	s.bme.Sense(&em)
+	logger.Debugf("BME: %8s %10s %9s\n", em.Temperature, em.Pressure, em.Humidity)
+	
 	wd := webdata {
-		Temp: sd.Temp,
-		Temp1: t1,
-		Humidity: sd.Humidity,
-		Pressure: sd.Pressure,
+		Temp: em.Temperature.Celsius(),
+		Temp1: e.Temperature.Celsius(),
+		Humidity: float64(em.Humidity) / float64(physic.PercentRH),
+		Pressure: float64(em.Pressure) / (10 * float64(physic.Pascal)),
 	}
-	json.NewEncoder(w).Encode(wd)
+
+	js, err := json.Marshal(wd)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(js)
 }
 
-func initMCP9808() *mcp9808.Dev {
+func initMCP9808() (*mcp9808.Dev, *bmxx80.Dev, i2c.BusCloser) {
 	if _, err := host.Init(); err != nil {
 		logger.Fatal(err)
 	}
@@ -78,53 +77,26 @@ func initMCP9808() *mcp9808.Dev {
 
 	flag.Parse()
 
-	logger.Info("Starting MCP9808 Temperature Sensor")
-	if _, err := host.Init(); err != nil {
-		logger.Fatal(err)
-	}
-
 	// Open default I²C bus.
 	bus, err := i2creg.Open(*i2cbus)
 	if err != nil {
 		logger.Fatalf("failed to open I²C: %v", err)
 	}
-	defer bus.Close()
+	
+	logger.Info("Starting BMP280 reader...")
+	d, err := bmxx80.NewI2C(bus, 0x76, &bmxx80.DefaultOpts)
+	if err != nil {
+		logger.Fatalf("failed to initialize bme280: %v", err)
+	}
 
+
+	logger.Info("Starting MCP9808 Temperature Sensor")
+	
 	// Create a new temperature sensor a sense with default options.
 	sensor, err := mcp9808.New(bus, &mcp9808.Opts{Addr: *address})
 	if err != nil {
 		logger.Fatalf("failed to open new sensor: %v", err)
 	}
-	return sensor
+	return sensor, d, bus
 }
 
-func getTemp(sensor *mcp9808.Dev) float64 {
-	
-	t, err := sensor.SenseTemp()
-	if err != nil {
-		logger.Errorf("sensor reading error: %v", err)
-		return -1000.0
-	}
-
-	// Read values from sensor every second.
-	// everySecond := time.Tick(time.Second)
-	// var halt = make(chan os.Signal, 1)
-	// signal.Notify(halt, syscall.SIGTERM)
-	// signal.Notify(halt, syscall.SIGINT)
-
-	// logger.Info("ctrl+c to exit")
-	// for {
-	// 	select {
-	// 	case <-everySecond:
-	// 		t, err := sensor.SenseTemp()
-	// 		if err != nil {
-	// 			return logger.Fatalf("sensor reading error: %v", err)
-	// 		}
-	// 		logger.Info(t)
-
-	// 	case <-halt:
-	// 		return nil
-	// 	}
-	// }
-	return t.Celsius()
-}
