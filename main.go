5d03801f1161b2ae357a2e6934723c120147340f
pointer2null <pointer2null@gmail.com>
2020-11-07 16:11:16 +0000

    dded adc wind sensor handling

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 	"periph.io/x/periph/conn/i2c/i2creg"
 	"periph.io/x/periph/conn/physic"
 	"periph.io/x/periph/devices/bmxx80"
+	"periph.io/x/periph/experimental/devices/ads1x15"
 	"periph.io/x/periph/experimental/devices/mcp9808"
 	"periph.io/x/periph/host"
 
@@ -25,40 +26,45 @@ import (
 
 const (
 	mmPerBucket float64 = 0.3
-	hgToPa float64 = 133.322387415
+	hgToPa      float64 = 133.322387415
 )
 
 type sensors struct {
-	mcp         *mcp9808.Dev
-	bme         *bmxx80.Dev
-	btips      []int
-	count        int				// GPIO bucket tip counter
-	lastTip      time.Time			// Last bucket tip
-	windticks    int				// GPIO wind speed tick counter
-	windTicks250 int				// number of ticks per 250ms
-	bus          *i2c.BusCloser
-	rainpin      *gpio.PinIO
-	windpin      *gpio.PinIO
-	rainHr       float64
-	pressure     float64
-	pressureHg   float64
-	humidity     float64
-	temp         float64
-	rain24     []float64
-	pressure24 []float64
-	humidity24 []float64
-	temp24     []float64
+	mcp           *mcp9808.Dev
+	bme           *bmxx80.Dev
+	btips         []int
+	count         int       // GPIO bucket tip counter
+	lastTip       time.Time // Last bucket tip
+	windticks     int       // GPIO wind speed tick counter
+	bus           *i2c.BusCloser
+	rainpin       *gpio.PinIO
+	windpin       *gpio.PinIO
+	windDir       *ads1x15.PinADC
+	rainHr        float64
+	pressure      float64
+	pressureHg    float64
+	humidity      float64
+	temp          float64
+	windSpeed     float64
+	windDirection float64
+	windVolts     float64
+	rain24        []float64
+	pressure24    []float64
+	humidity24    []float64
+	temp24        []float64
 }
 
 type webdata struct {
-	TimeNow    string    `json:"time"`
-	Temp       float64   `json:"temp_C"`
-	Humidity   float64   `json:"humidity_RH"`
-	Pressure   float64   `json:"pressure_hPa"`
-	PressureHg float64   `json:"pressure_mmHg"`
-	RainHr     float64   `json:"rain_mm_hr"`
-	LastTip    string    `json:"last_tip"`
-	Wind       int       `json:"wind"`
+	TimeNow    string  `json:"time"`
+	Temp       float64 `json:"temp_C"`
+	Humidity   float64 `json:"humidity_RH"`
+	Pressure   float64 `json:"pressure_hPa"`
+	PressureHg float64 `json:"pressure_mmHg"`
+	RainHr     float64 `json:"rain_mm_hr"`
+	LastTip    string  `json:"last_tip"`
+	Wind       int     `json:"wind"`
+	WindDir    float64 `json:"wind_dir"`
+	WindVolts  float64 `json:"wind_volt"`
 }
 
 type webHistory struct {
@@ -70,45 +76,52 @@ type webHistory struct {
 }
 
 var atmPresure = prometheus.NewGauge(
-    prometheus.GaugeOpts{
-        Name: "atmospheric_pressure",
-        Help: "Atmospheric pressure hPa",
-    },
+	prometheus.GaugeOpts{
+		Name: "atmospheric_pressure",
+		Help: "Atmospheric pressure hPa",
+	},
 )
 
 var mmRainPerHour = prometheus.NewGauge(
-    prometheus.GaugeOpts{
-        Name: "mm_rain_last_hour",
-        Help: "mm of Rain in the last hour",
-    },
+	prometheus.GaugeOpts{
+		Name: "mm_rain_last_hour",
+		Help: "mm of Rain in the last hour",
+	},
 )
 
 var mmRainPerMin = prometheus.NewGauge(
-    prometheus.GaugeOpts{
-        Name: "mm_rain_last_min",
-        Help: "mm of Rain in the last min",
-    },
+	prometheus.GaugeOpts{
+		Name: "mm_rain_last_min",
+		Help: "mm of Rain in the last min",
+	},
 )
 
 var rh = prometheus.NewGauge(
-    prometheus.GaugeOpts{
-        Name: "relative_humidity",
-        Help: "Relative Humidity",
-    },
+	prometheus.GaugeOpts{
+		Name: "relative_humidity",
+		Help: "Relative Humidity",
+	},
 )
 
 var temperature = prometheus.NewGauge(
-    prometheus.GaugeOpts{
-        Name: "temperature",
-        Help: "Temperature C",
-    },
+	prometheus.GaugeOpts{
+		Name: "temperature",
+		Help: "Temperature C",
+	},
 )
 
 var windspeed = prometheus.NewGauge(
-    prometheus.GaugeOpts{
-        Name: "windspeed",
-        Help: "Wind Speed m/s",
-    },
+	prometheus.GaugeOpts{
+		Name: "windspeed",
+		Help: "Wind Speed m/s",
+	},
+)
+
+var windDirection = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Name: "winddirection",
+		Help: "Wind Direction Deg",
+	},
 )
 
 // called by prometheus
@@ -146,12 +159,12 @@ func main() {
 	logger.Fatal(http.ListenAndServe(":80", nil))
 }
 
-func (s *sensors) history (w http.ResponseWriter, r *http.Request) {
+func (s *sensors) history(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	h := webHistory {
-		Temp24H: s.temp24,
-		Rain24H: s.rain24,
-		TipHistory: s.btips,
+	h := webHistory{
+		Temp24H:     s.temp24,
+		Rain24H:     s.rain24,
+		TipHistory:  s.btips,
 		Pressure24H: s.pressure24,
 		Humidity24H: s.humidity24,
 	}
@@ -175,7 +188,9 @@ func (s *sensors) handler(w http.ResponseWriter, r *http.Request) {
 		RainHr:     s.getMMLastHour(),
 		LastTip:    s.lastTip.Format(time.RFC822),
 		TimeNow:    time.Now().Format(time.RFC822),
-		Wind:		s.windTicks250,
+		Wind:       s.windticks,
+		WindDir:    s.windDirection,
+		WindVolts:  s.windVolts,
 	}
 
 	js, err := json.Marshal(wd)
@@ -186,7 +201,7 @@ func (s *sensors) handler(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
-func (s *sensors) initSensors() () {
+func (s *sensors) initSensors() {
 	if _, err := host.Init(); err != nil {
 		logger.Error("Failed to init i2c bus")
 		logger.Fatal(err)
@@ -239,6 +254,20 @@ func (s *sensors) initSensors() () {
 		logger.Error(err)
 	}
 
+	logger.Info("Starting Wind direction ADC")
+	// Create a new ADS1115 ADC.
+	adc, err := ads1x15.NewADS1115(bus, &ads1x15.DefaultOpts)
+	if err != nil {
+		logger.Error(err)
+	}
+
+	// Obtain an analog pin from the ADC.
+	dirPin, err := adc.PinForChannel(ads1x15.Channel0, 5*physic.Volt, 1*physic.Hertz, ads1x15.SaveEnergy)
+	if err != nil {
+		logger.Error(err)
+	}
+	defer dirPin.Halt()
+
 	s.bme = bme
 	s.mcp = sensor
 	s.btips = make([]int, 60)
@@ -250,6 +279,7 @@ func (s *sensors) initSensors() () {
 	s.bus = &bus
 	s.rainpin = &rainpin
 	s.windpin = &windpin
+	s.windDir = &dirPin
 }
 
 func (s *sensors) monitorRainGPIO() {
@@ -287,11 +317,10 @@ of ten minutes. Terms such as 'severe gale', 'storm', etc are also used to
 describe winds of 41 knots or greater.
 */
 
-func (s *sensors) readWindsSpeedHF(){
-	for range time.Tick(250 * time.Millisecond) {
-		s.windTicks250 = s.windticks // windticks250 is so we can check the output directly - will remove all these local web values at some point I think
+func (s *sensors) readWindsSpeedHF() {
+	for range time.Tick(time.Second) {
 		s.windticks = 0
-		windspeed.Set(float64(s.windTicks250))
+		windspeed.Set(float64(s.windticks))
 	}
 }
 
@@ -308,13 +337,22 @@ func (s *sensors) measureSensors() {
 	}
 	logger.Debugf("BME: %8s %10s %9s\n", em.Temperature, em.Pressure, em.Humidity)
 	s.humidity = math.Round(float64(em.Humidity) / float64(physic.PercentRH))
-	s.pressure = math.Round(float64(em.Pressure) / float64(100 * physic.Pascal))
+	s.pressure = math.Round(float64(em.Pressure) / float64(100*physic.Pascal))
 	s.pressureHg = math.Round(float64(em.Pressure) / (float64(physic.Pascal) * hgToPa))
 	s.temp = e.Temperature.Celsius()
 	if s.temp < 50 { // use low res sensor, prevents -273.15 when sensor is offline!
 		s.temp = em.Temperature.Celsius()
 	}
-    // prometheus data
+
+	sample, err := (*s.windDir).Read()
+	if err != nil {
+		logger.Errorf("Error reading wind direction value [%v]", err)
+		sample.Raw = 0
+	}
+	s.windVolts = float64(sample.V) / float64(physic.Volt)
+	s.windDirection = voltToDegrees(s.windVolts)
+	logger.Debugf("Volt [%v], Dir [%v]", s.windVolts, s.windDirection)
+	// prometheus data
 	mmRainPerHour.Set(s.getMMLastHour())
 	atmPresure.Set(s.pressure)
 	rh.Set(s.humidity)
@@ -322,6 +360,44 @@ func (s *sensors) measureSensors() {
 	mmRainPerMin.Set(s.getMMLastMin())
 }
 
+func voltToDegrees(v float64) float64 {
+	// this is based on the sensor datasheet that gives a list of voltages for each direction when set up according
+	// to the circuit given
+	switch {
+	case v < 0.365:
+		return 112.5
+	case v < 0.430:
+		return 67.5
+	case v < 0.535:
+		return 90.0
+	case v < 0.760:
+		return 157.5
+	case v < 1.045:
+		return 135.0
+	case v < 1.295:
+		return 202.5
+	case v < 1.690:
+		return 180.0
+	case v < 2.115:
+		return 22.5
+	case v < 2.590:
+		return 45.0
+	case v < 3.005:
+		return 247.5
+	case v < 3.225:
+		return 225.0
+	case v < 3.635:
+		return 337.5
+	case v < 3.940:
+		return 0
+	case v < 4.185:
+		return 292.5
+	case v < 4.475:
+		return 315.0
+	default:
+		return 270.0
+	}
+}
 
 func (s *sensors) recordHistory() {
 	for x := range time.Tick(time.Minute) {
@@ -348,18 +424,18 @@ func (s *sensors) getMMLastHour() float64 {
 	for _, x := range s.btips {
 		total += x
 	}
-	return math.Round(float64(total) * mmPerBucket * 100) / 100
+	return math.Round(float64(total)*mmPerBucket*100) / 100
 }
 
 func (s *sensors) getMMLastMin() float64 {
-		min := time.Now().Minute()
+	min := time.Now().Minute()
 	count := 0
 	if min >= 2 {
-		count = s.count + s.btips[min - 1] + s.btips[min - 2]
+		count = s.count + s.btips[min-1] + s.btips[min-2]
 	} else if min == 1 {
 		count = s.count + s.btips[0] + s.btips[59]
-	} else if min ==  0 {
+	} else if min == 0 {
 		count = s.count + s.btips[59] + s.btips[58]
 	}
-	return (float64(count)/3 * mmPerBucket)
+	return (float64(count) / 3 * mmPerBucket)
 }
